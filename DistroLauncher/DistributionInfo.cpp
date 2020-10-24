b379e995d145e1df018c37cee394e69c2a8e6646
Алексей Тулинов <aleksey.tulinov@gmail.com>
2020-10-24 21:36:43 +0300

    +minor refactoring +mirror setup

--- DistroLauncher/DistributionInfo.cpp
+++ DistroLauncher/DistributionInfo.cpp
@@ -4,32 +4,53 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
 bool DistributionInfo::CreateUser(std::wstring_view userName)
 {
+    const auto userNameStr = std::wstring(userName);
+
     // Create the user account.
     DWORD exitCode;
-    const std::wstring createUserCommandLine = L"/usr/sbin/useradd -m " + std::wstring(userName);
+    const std::wstring createUserCommandLine = L"/usr/sbin/useradd -m " + userNameStr;
     HRESULT hr = g_wslApi.WslLaunchInteractive(createUserCommandLine.c_str(), true, &exitCode);
     if ((FAILED(hr)) || (exitCode != 0)) {
         return false;
     }
 
+    const std::wstring mirrorURL = L"https://mirror.yandex.ru/mirrors/voidlinux";
+    const std::wstring mirrorsDocURL = L"https://docs.voidlinux.org/xbps/repositories/mirrors/index.html";
+
     const std::wstring postUserCommands[] = {
-        L"/usr/sbin/passwd " + std::wstring(userName),
-        L"/usr/sbin/usermod -aG adm,wheel,floppy,cdrom,tape " + std::wstring(userName),
+        // Set user pasword, so `sudo` can be used
+        // (but not `su` because there is no root password)
+        L"/usr/sbin/passwd " + userNameStr,
+        // Important group to set is `wheel`
+        L"/usr/sbin/usermod -aG adm,wheel,floppy,cdrom,tape " + userNameStr,
+        // Add wheel group to sudoers. `sudo` is already installed
         LR"(echo "%wheel ALL=(ALL) ALL" >/etc/sudoers.d/wheel)",
+        // Regenerate locales so default one (`en_US.UTF-8`) is properly initialized
+        L"/usr/sbin/xbps-reconfigure -f glibc-locales",
+        // Setup mirror
+        L"mkdir -p /etc/xbps.d",
+        L"cp /usr/share/xbps.d/*-repository-*.conf /etc/xbps.d/",
+        LR"(sed -i 's|https://alpha.de.repo.voidlinux.org|)" + mirrorURL + LR"(|g' /etc/xbps.d/*-repository-*.conf)",
     };
 
     for (const auto& postUserCommandLine : postUserCommands) {
         hr = g_wslApi.WslLaunchInteractive(postUserCommandLine.c_str(), true, &exitCode);
         if ((FAILED(hr)) || (exitCode != 0)) {
             // Delete the user if any command failed
-            const std::wstring deleteUserCommandLine = L"/usr/sbin/userdel " + std::wstring(userName);
+            const std::wstring deleteUserCommandLine = L"/usr/sbin/userdel " + userNameStr;
             g_wslApi.WslLaunchInteractive(deleteUserCommandLine.c_str(), true, &exitCode);
             return false;
         }
     }
+
+    std::wcout << std::endl
+        << L"Your mirror is " + mirrorURL << std::endl
+        << L"For more information on mirrors visit " + mirrorsDocURL << std::endl
+        << std::endl;
 
     return true;
 }
